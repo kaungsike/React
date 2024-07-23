dbf4053caa063f2ce96c8774fa0f34c60c8ba17d
kaungsike <kaungsike9@gmail.com>
2024-07-23 20:08:55 +0630

    Learn React

--- onlineShop/src/components/Rating.jsx
+++ onlineShop/src/components/Rating.jsx
@@ -1,31 +1,31 @@
-import React from 'react'
+import React from "react";
 
-const Rating = ({rate}) => {
+const Rating = ({ rate }) => {
+  console.log(rate);
 
-    const stars = [];
-let i = 0
-    for (i; i < 5; i++) {
-      stars.push(
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth={1.5}
-          stroke="currentColor"
-          className="size-6"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
-          />
-        </svg>
-      );
-    }
+  const stars = [];
 
-  return (
-    <div className="mt-auto flex">{stars}</div>
-  )
-}
+  for (let i = 1; i <= 5; i++) {
+    stars.push(
+      <svg
+        key={i}
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className={`size-5 stroke-yellow-400 ${rate >= i && "fill-yellow-400"}`}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z"
+        />
+      </svg>
+    );
+  }
 
-export default Rating
+  return <div className="mt-auto flex">{stars}</div>;
+};
+
+export default Rating;
